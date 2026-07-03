2f6b6138af18be1ffb354a511353d0959d824b37
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-03 14:06:06 -0300

    feat: add UpdateUserControlle

---
+++ src/controllers/update-user.js
@@ -0,0 +1,69 @@
+import { badRequest, ok, serverError } from './helpers.js'
+import validator from 'validator'
+import { UpdateUserUseCase } from '../use-cases/update-user.js'
+import { EmailAlreadyInUseError } from '../errors/user.js'
+
+export class UpdateUserController {
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId
+            const isIdValid = validator.isUUID(userId)
+            if (!isIdValid) {
+                return badRequest({
+                    message: 'The provided user ID is not valid'
+                })
+            }
+
+            const updateUserParams = httpRequest.body
+            const allowedFields = [
+                'first_name',
+                'last_name',
+                'email',
+                'password'
+            ]
+
+            const someFieldIsNotAllowed = Object.keys(updateUserParams).some(
+                (field) => !allowedFields.includes(field)
+            )
+
+            if (someFieldIsNotAllowed) {
+                return badRequest({
+                    message: 'Some provided fields are not allowed'
+                })
+            }
+
+            if (updateUserParams.password) {
+                const passwordIsNotValid = updateUserParams.password.length < 6
+                if (passwordIsNotValid) {
+                    return badRequest({
+                        message: 'Password must be at least 6 characters long'
+                    })
+                }
+            }
+
+            if (updateUserParams.email) {
+                const emailIsValid = validator.isEmail(updateUserParams.email)
+                if (!emailIsValid) {
+                    return badRequest({
+                        message:
+                            'Invalid e-mail address. Please provide a valid one'
+                    })
+                }
+            }
+            const updateUserUseCase = new UpdateUserUseCase()
+            const updatedUser = await updateUserUseCase.execute(
+                userId,
+                updateUserParams
+            )
+            return ok(updatedUser)
+        } catch (error) {
+            if (error instanceof EmailAlreadyInUseError) {
+                return badRequest({
+                    message: error.message
+                })
+            }
+            console.error(error)
+            return serverError()
+        }
+    }
+}
