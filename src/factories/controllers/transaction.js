692869aa04f695820600ef167095c97985e9de89
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-07 10:14:19 -0300

    feat: add CreateTransactionController factory

---
+++ src/factories/controllers/transaction.js
@@ -0,0 +1,17 @@
+import { CreateTransactionController } from '../../controllers/index.js'
+import {
+    PostgresCreateTransactionRepository,
+    PostgresGetUserByIdRepository
+} from '../../repositories/postgres/index.js'
+import { CreateTransactionUseCase } from '../../use-cases/index.js'
+
+export const makeCreateTransactionController = () => {
+    const getUserByIdRepository = new PostgresGetUserByIdRepository()
+    const createTransactionRepository =
+        new PostgresCreateTransactionRepository()
+    const createTransactionUseCase = new CreateTransactionUseCase(
+        getUserByIdRepository,
+        createTransactionRepository
+    )
+    return new CreateTransactionController(createTransactionUseCase)
+}
