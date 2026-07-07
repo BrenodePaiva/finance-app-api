199ee65084969170079a06fd3a2ab9be7659a389
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-07 15:50:02 -0300

    feat: add requiredFieldsIsMissing function

--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -8,6 +8,12 @@ export const invalidIdResponse = () =>
         message: 'The provided user ID is not valid'
     })
 
+export const requiredFieldIsMissingResponse = (field) => {
+    return badRequest({
+        message: `The field ${field} is required.`
+    })
+}
+
 export const checkIfIsString = (value) => typeof value === 'string'
 
 export const validateRequiredFields = (params, requiredFields) => {
--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -6,6 +6,7 @@ import {
     created,
     invalidEmailResponse,
     invalidPasswordResponse,
+    requiredFieldIsMissingResponse,
     serverError,
     validateRequiredFields
 } from '../helpers/index.js'
@@ -28,9 +29,7 @@ export class CreateUserController {
                 validateRequiredFields(params, requiredFields)
 
             if (!requiredFieldsWereProvided) {
-                return badRequest({
-                    message: `The field ${missingField} is required`
-                })
+                return requiredFieldIsMissingResponse(missingField)
             }
 
             const passwordIsValid = checkIfPasswordIsValid(params.password)
