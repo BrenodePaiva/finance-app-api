b367143bc80926761ad7d49e3adc4b7c1cb4bf26
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-08 16:05:45 -0300

    feat: add DeleteTransactionUseCase

---
+++ src/use-cases/transaction/delete-transaction.js
@@ -0,0 +1,8 @@
+export class DeleteTransactionUseCase {
+    constructor(deleteTransactionRepository) {
+        this.deleteTransactionRepository = deleteTransactionRepository
+    }
+    async execute(transactionId) {
+        return await this.deleteTransactionRepository.execute(transactionId)
+    }
+}
