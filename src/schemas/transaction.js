3bc32bf09f7ef5a1935d828306dc847d9f128e68
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-10 14:37:48 -0300

    feat: add zod to CreateTransactionController

---
+++ src/schemas/transaction.js
@@ -0,0 +1,23 @@
+import z from 'zod'
+import validator from 'validator'
+
+export const createTransactionSchema = z.object({
+    user_id: z.uuid({ error: 'User ID must be a valid UUID.' }),
+    name: z
+        .string({ error: 'Name is required.' })
+        .min(1, { error: 'Name is required.' }),
+    date: z.iso.date({ error: 'Date must be a valid date.' }),
+    type: z.enum(['EARNING', 'EXPENSE', 'INVESTMENT'], {
+        error: 'The type must be EARNING, EXPENSE or INVESTMENT.'
+    }),
+    amount: z
+        .number({ error: 'Amount must be a number.' })
+        .min(1, { error: 'Amount must be greater than 0.' })
+        .refine((value) =>
+            validator.isCurrency(value.toFixed(2), {
+                digits_after_decimal: [2],
+                allow_negatives: false,
+                decimal_separator: '.'
+            })
+        )
+})
