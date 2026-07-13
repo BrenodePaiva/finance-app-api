1f48f7aae49f2ad483fca95d935437746586c12b
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-13 09:37:24 -0300

    feat: ensure CreateUserRepository and GetUserByEmailRepository use Prisma

--- prisma/prisma.js
+++ prisma/prisma.js
@@ -1,3 +1,9 @@
-import { PrismaClient } from '@prisma/client/extension'
+import 'dotenv/config.js'
+import { PrismaPostgresAdapter } from '@prisma/adapter-ppg'
+import { PrismaClient } from '../src/generated/prisma/client.ts'
 
-export const prisma = new PrismaClient()
+const adapter = new PrismaPostgresAdapter({
+    connectionString: process.env.DATABASE_URL
+})
+
+export const prisma = new PrismaClient({ adapter })
