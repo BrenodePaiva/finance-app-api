a2e15281b22073c190536188228711a3710b7691
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-14 10:34:45 -0300

    test: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -30,4 +30,26 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(201)
         expect(result.body).toBe(httpRequest.body)
     })
+
+    it('should return 400 if first_name is not provided', async () => {
+        // arrange
+        const createUserUseCaseStub = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(
+            createUserUseCaseStub
+        )
+
+        const httpRequest = {
+            body: {
+                last_name: 'Jest',
+                email: 'teste@jest.com',
+                password: '1234567'
+            }
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
