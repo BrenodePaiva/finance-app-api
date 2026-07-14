d4364e31d4744522cba591bbac158e0c354c8cb6
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-14 16:23:54 -0300

    test: ensure CreateUserController returns 500 if CreateUserUseCase throws

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -190,4 +190,33 @@ describe('Create User Controller', () => {
         // assert
         expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
     })
+
+    it('should return 500 if CreateUserUseCase throws', async () => {
+        //   arrange
+        const createUserUseCaseStub = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(
+            createUserUseCaseStub
+        )
+
+        const httpRequest = {
+            body: {
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({ length: 7 })
+            }
+        }
+
+        jest.spyOn(createUserUseCaseStub, 'execute').mockImplementationOnce(
+            () => {
+                throw new Error()
+            }
+        )
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(500)
+    })
 })
