0cbf6e3cb8af56be446ee5a8d6d0e3fe7943d877
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-16 11:08:23 -0300

    test: ensure UpdateTransactionController returns 400 when type is invalid

--- src/controllers/transaction/update-transaction.test.js
+++ src/controllers/transaction/update-transaction.test.js
@@ -93,4 +93,21 @@ describe('UpdateTransactioController', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when type is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                type: 'invalid_type'
+            }
+        })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
