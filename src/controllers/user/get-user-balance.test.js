0784faf1c4f7300cc2e9b57553df72d6ea74f20f
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-16 14:01:18 -0300

    test: ensure GetUserBalanceController calls GetUserBalanceUseCase with correct params

--- src/controllers/user/get-user-balance.test.js
+++ src/controllers/user/get-user-balance.test.js
@@ -58,4 +58,16 @@ describe('GetUserBalanceController', () => {
         // assert
         expect(result.statusCode).toBe(500)
     })
+
+    it('should call GetUserBalanceUseCase with correct params', async () => {
+        // arrange
+        const { sut, getUserBalanceUseCaseStub } = makeSut()
+        const executeSpy = jest.spyOn(getUserBalanceUseCaseStub, 'execute')
+
+        // act
+        await sut.execute(httpRequest)
+
+        // assert
+        expect(executeSpy).toHaveBeenCalledWith(httpRequest.params.userId)
+    })
 })
