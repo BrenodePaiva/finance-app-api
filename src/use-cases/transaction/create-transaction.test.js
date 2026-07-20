90d656189a38c08e7f8ffee3a279a296c5977d76
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-20 11:56:22 -0300

    test: ensure CreateTransactionUseCase throws if IdGeneratorAdapter throws

--- src/use-cases/transaction/create-transaction.test.js
+++ src/use-cases/transaction/create-transaction.test.js
@@ -137,4 +137,20 @@ describe('CreateTransactionUseCase', () => {
         // assert
         expect(promise).rejects.toThrow()
     })
+
+    it('should throw if IdGeneratorAdapter throws', async () => {
+        // arrange
+        const { sut, idGeneratorAdapterStub } = makeSut()
+        jest.spyOn(idGeneratorAdapterStub, 'execute').mockImplementationOnce(
+            () => {
+                throw new Error()
+            }
+        )
+
+        // act
+        const promise = sut.execute(params)
+
+        // assert
+        expect(promise).rejects.toThrow()
+    })
 })
