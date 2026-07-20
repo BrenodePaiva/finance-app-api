f8cd7e2d8847ba184bbdd885844bc3b946697eff
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-20 11:32:11 -0300

    test: ensure CreateTransactionUseCase creates transaction successfully

---
+++ src/use-cases/transaction/create-transaction.test.js
@@ -0,0 +1,72 @@
+import { faker } from '@faker-js/faker'
+import { CreateTransactionUseCase } from './create-transaction'
+
+describe('CreateTransactionUseCase', () => {
+    const user = {
+        id: faker.string.uuid(),
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: faker.internet.password({ length: 7 })
+    }
+
+    class GetUserByIdRepositoryStub {
+        async execute(userId) {
+            return { ...user, id: userId }
+        }
+    }
+
+    class IdGeneratorAdapterStub {
+        execute() {
+            return 'random_id'
+        }
+    }
+
+    class CreateTransactionRepositoryStub {
+        async execute(transaction) {
+            return transaction
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByIdRepositoryStub = new GetUserByIdRepositoryStub()
+
+        const idGeneratorAdapterStub = new IdGeneratorAdapterStub()
+
+        const createTransactionRepositoryStub =
+            new CreateTransactionRepositoryStub()
+
+        const sut = new CreateTransactionUseCase(
+            getUserByIdRepositoryStub,
+            idGeneratorAdapterStub,
+            createTransactionRepositoryStub
+        )
+
+        return {
+            sut,
+            getUserByIdRepositoryStub,
+            idGeneratorAdapterStub,
+            createTransactionRepositoryStub
+        }
+    }
+
+    const params = {
+        id: faker.string.uuid(),
+        user_id: faker.string.uuid(),
+        name: faker.string.alpha(10),
+        date: faker.date.anytime().toISOString(),
+        amount: Number(faker.finance.amount()),
+        type: 'EXPENSE'
+    }
+
+    it('should create transaction successfully', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute(params)
+
+        // assert
+        expect(result).toEqual({ ...params, id: 'random_id' })
+    })
+})
