7d83a334492b057a84054a8b0d559f4fbb10379b
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-22 11:13:26 -0300

    chore: add waitForPostgres function to ensure Postgres readiness before tests

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -1,7 +1,33 @@
 import { execSync } from 'child_process'
 
+function waitForPostgres() {
+    let ready = false
+    let attempts = 0
+
+    while (!ready && attempts < 10) {
+        try {
+            // roda o pg_isready dentro do container
+            execSync(
+                'docker exec finance-app-test pg_isready -U root -h localhost -p 5432'
+            )
+            ready = true
+            // eslint-disable-next-line no-unused-vars
+        } catch (err) {
+            attempts++
+            console.log(`Postgres ainda não está pronto, tentativa ${attempts}`)
+            // espera 2 segundos antes de tentar de novo
+            execSync('sleep 2')
+        }
+    }
+
+    if (!ready) {
+        throw new Error('Postgres não ficou pronto a tempo')
+    }
+}
+
 async function init() {
     execSync('docker compose up -d --wait postgres-test')
+    waitForPostgres()
     execSync('npx prisma db push')
 }
 
