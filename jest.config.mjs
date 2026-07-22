3d05c296d0d2833847c8e1dfe037eaf582f36221
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-22 10:18:37 -0300

    chore: add jest global setup file to run test db container before all tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -5,6 +5,7 @@
 
 /** @type {import('jest').Config} */
 const config = {
+    globalSetup: '<rootDir>/jest.global-setup.js',
     setupFiles: ['./jest.setup.js'],
     transform: {
         '^.+\\.[jt]sx?$': 'babel-jest'
