5f0ac1a4cd94cda3855a6a0eb7864891d0b01933
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-24 10:00:26 -0300

    test: ensure DeleteTransactionRepository throws TransactionNotFoundError if transaction is not found

--- src/repositories/postgres/transaction/delete-transaction.test.js
+++ src/repositories/postgres/transaction/delete-transaction.test.js
@@ -2,6 +2,8 @@ import dayjs from 'dayjs'
 import { prisma } from '../../../../prisma/prisma'
 import { transaction, user } from '../../../tests'
 import { PostgresDeleteTransactionRepository } from './delete-transaction'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client'
+import { TransactionNotFoundError } from '../../../errors'
 
 describe('PostgresDeleteTransactionRepository', () => {
     it('should delete a transaction on db', async () => {
@@ -49,5 +51,20 @@ describe('PostgresDeleteTransactionRepository', () => {
         const promise = sut.execute(transaction.id)
 
         expect(promise).rejects.toThrow()
+    })
+
+    it('should throw TransactionNotFoundError if transaction is not found', async () => {
+        const sut = new PostgresDeleteTransactionRepository()
+        jest.spyOn(prisma.transaction, 'delete').mockRejectedValueOnce(
+            new PrismaClientKnownRequestError('', {
+                code: 'P2025'
+            })
+        )
+
+        const promise = sut.execute(transaction.id)
+
+        expect(promise).rejects.toThrow(
+            new TransactionNotFoundError(transaction.id)
+        )
     })
 })
