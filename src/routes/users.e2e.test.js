923e0e466416de1073c153370136de8048559633
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-24 16:23:14 -0300

    test: ensure GET /api/users/:userId returns 200 when user is found

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -1,6 +1,6 @@
 import request from 'supertest'
-import { app } from '../..'
 import { user } from '../tests'
+import { app } from '../app'
 
 describe('User Router E2E Tests', () => {
     it('POST /api/users should return 201 when user is created', async () => {
@@ -12,5 +12,19 @@ describe('User Router E2E Tests', () => {
             })
 
         expect(response.status).toBe(201)
+    })
+
+    it('GET /api/users/:userId should return 200 when user is found', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined
+            })
+
+        const response = await request(app).get(`/api/users/${createdUser.id}`)
+
+        expect(response.status).toBe(200)
+        expect(response.body).toEqual(createdUser)
     })
 })
