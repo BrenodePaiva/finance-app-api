cefa6b79da83b50ef91e3cbb11a4e0b7e7d8e5b8
BrenodePaiva <brenopaiva552@gmail.com>
2026-07-27 13:45:34 -0300

    test: ensure POST /api/users returns 400 if the provider e-mail is already in use

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -149,4 +149,23 @@ describe('User Router E2E Tests', () => {
 
         expect(response.status).toBe(404)
     })
+
+    it('POST /api/users should return 400 when the provider e-mail is already in use', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined
+            })
+
+        const response = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+                email: createdUser.email
+            })
+
+        expect(response.status).toBe(400)
+    })
 })
