4e21e737ddb962a3d90017b0cf18349fbb211112
BrenodePaiva <brenopaiva552@gmail.com>
2026-08-04 10:29:13 -0300

    test: add login user route

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -168,4 +168,22 @@ describe('User Router E2E Tests', () => {
 
         expect(response.status).toBe(400)
     })
+
+    it('POST /api/users/login should return 200 and tokens when user credential are valid', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined
+            })
+
+        const response = await request(app).post('/api/users/login').send({
+            email: createdUser.email,
+            password: user.password
+        })
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.tokens.accessToken).toBeDefined()
+        expect(response.body.tokens.refreshToken).toBeDefined()
+    })
 })
